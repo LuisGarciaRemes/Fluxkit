f1813d961ac4074bfcc53ea76046d5861c0cd196
lopackuub <36348201+lopackuub@users.noreply.github.com>
2018-10-22 19:35:27 -0600

    Slicing Mechanic started

---
+++ Test3/Assets/Scripts/CutPointScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutPointScript : MonoBehaviour {
+
+    public GameObject point;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Knife"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+ }
---
+++ Test3/Assets/Scripts/SlicingScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlicingScript : MonoBehaviour {
+
+    public SphereCollider[] points;
+    private bool allPointsCut;
+
+	// Use this for initialization
+	void Start () {      
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        allPointsCut = true;
+        for (int i = 0; i < points.Length; i++)
+        {
+            if(points[i].enabled)
+            {
+                allPointsCut = false;
+            }
+        }
+
+        if(allPointsCut)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
