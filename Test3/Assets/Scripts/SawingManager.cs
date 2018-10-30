a541b015fd0f0e86bbcaefba7667d5cb90439228
AN ZHU <zhua@kean.edu>
2018-10-30 11:46:08 -0600

    Sawing Machenic done

---
+++ Test3/Assets/Scripts/SawingManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawingManager : MonoBehaviour {
+    public static SawingManager Instance = null;
+    public Transform contact_position = null;
+    public GameObject Foot = null;
+    public Vector3 NormalVector;
+    public Vector3 Privious_contact_Position;
+    public bool registered = false;
+    public float max_tolerence;
+    public float current_tolerence;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public bool Register(Transform contact_position,GameObject Foot) {
+        this.Foot = Foot;
+        this.contact_position = contact_position;
+        this.registered = true;
+        this.Privious_contact_Position = contact_position.position;
+        current_tolerence = max_tolerence;
+        return true;
+    }
+    public bool Resign() {
+        this.contact_position = null;
+        this.registered = false;
+        this.Foot = null;
+        this.current_tolerence = 0;
+        return true;
+    }
+    public static float LenProjAB(Vector3 a, Vector3 b) {
+        float result = a.x * b.x + a.y * b.y + a.z * b.z;
+        return Mathf.Abs(result);
+    }
+    private void Update()
+    {
+        if (registered) {
+            Vector3 delta = contact_position.position - Privious_contact_Position;
+            Privious_contact_Position = contact_position.position;
+            current_tolerence -= Mathf.Min(0.6f, LenProjAB(NormalVector, delta))*Time.deltaTime;
+            if (current_tolerence < 0) {
+                //The foot is cut off do something
+                this.Foot = null;
+                this.Resign();
+            }
+        }
+    }
+    
+}
--- Test3/Assets/Scripts/Tramble.cs
+++ Test3/Assets/Scripts/Tramble.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tramble : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Saw") {
+            SawingManager.Instance.Register(this.transform, this.gameObject);
+        }
+    }
     private void OnTriggerStay(Collider other)
     {
-        HoloToolkit.Unity.InputModule.Examples.Grabbables.GrabbableChild temp = other.gameObject.GetComponent<HoloToolkit.Unity.InputModule.Examples.Grabbables.GrabbableChild>();
-        if (temp != null) {
-            TestViberate.Instance.Viberate(temp.my_hand, 1.0f, 1.0f);
+        if (other.tag == "Saw")
+        {
+            HoloToolkit.Unity.InputModule.Examples.Grabbables.GrabbableChild temp = other.gameObject.GetComponent<HoloToolkit.Unity.InputModule.Examples.Grabbables.GrabbableChild>();
+            if (temp != null)
+            {
+                TestViberate.Instance.Viberate(temp.my_hand, 1.0f, 1.0f);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Saw") {
+            SawingManager.Instance.Resign();
         }
     }
 }
