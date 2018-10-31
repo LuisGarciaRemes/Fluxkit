bba7bae5d008bee16065c34a877319acb1ee10c5
AN ZHU <zhua@kean.edu>
2018-10-31 17:10:36 -0600

    Sound Effect Added

---
+++ Test3/Assets/Scripts/SoundEffectManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SoundEffectManager : MonoBehaviour {
+    [System.Serializable]
+    public struct SoundEffectSources
+    {
+        public string name;
+        public AudioClip source;
+    }
+    public SoundEffectSources[] soundEffectSource;
+    public static SoundEffectManager Instance;
+    // Use this for initialization
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void Play(string Name,Vector3 Postion)
+    {
+        SoundEffectSources temp = new SoundEffectSources();
+        foreach (SoundEffectSources t in soundEffectSource){
+            if (t.name == Name) {
+                temp = t;
+            }
+        }
+            AudioSource.PlayClipAtPoint(temp.source, Postion);
+    }
+    public void Play(string Name) {
+        SoundEffectSources temp = new SoundEffectSources();
+        foreach (SoundEffectSources t in soundEffectSource)
+        {
+            if (t.name == Name)
+            {
+                temp = t;
+            }
+        }
+        AudioSource.PlayClipAtPoint(temp.source, new Vector3(0, 0, 0));
+    }
+
+}
