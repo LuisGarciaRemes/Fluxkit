deec6c7c531e9db15210dbd5d7fcc1bcc84fb7c4
lopackuub <36348201+lopackuub@users.noreply.github.com>
2018-11-01 13:01:17 -0600

    An changes

--- Test3/Assets/Scripts/FootHolder.cs
+++ Test3/Assets/Scripts/FootHolder.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FootHolder : MonoBehaviour {
-
+    private Collider other = null;
+   
     // Use this for initialization
     private void OnTriggerEnter(Collider other)
     {
@@ -13,10 +14,14 @@ public class FootHolder : MonoBehaviour {
             {
                 Destroy(component);
             }
-            other.transform.parent = this.transform;
+            this.other = other;
             other.transform.position = new Vector3(0, 0, 0);
+            other.transform.localScale = new Vector3(.05f, 0.05f, 5f);
             other.transform.rotation = Quaternion.identity;
+            other.transform.Rotate(90f, 0f,0f);
+            other.transform.parent = this.transform;
             GameOver.Instance.nMouseOver();
         }
     }
+  
 }
